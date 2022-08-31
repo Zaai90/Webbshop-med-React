8889dead04049acc4371c585ffab8072f8342003
Zaai90 <erik.grune@gmail.com>
2022-08-31 15:57:33 +0200

    netlify file added

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
