ef0727dd820267545f3e6db55aeeeeca6dd36fb6
Tommy Zarén <85tomzar@skola.boras.se>
2022-08-31 20:57:24 +0200

    setup basic grid

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,23 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
 import MainContent from "../components/MainContent";
+import { products as procutsArr, Product } from "../ProductData";
 
 const Store = () => {
+  const [products, setProduct] = useState<Product[]>(procutsArr);
+
   return (
     <MainContent>
-      <div>Store</div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          {products.map((productCard) => (
+            <Grid bgcolor={"blue"} item xs={6} md={4}>
+              {productCard.designer}
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </MainContent>
   );
 };
