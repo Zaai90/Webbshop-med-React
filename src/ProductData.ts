6811c0665cd952ea42c398b30f597403a6ddb87f
Carl Åfalk <carl.afalk@hotmail.se>
2022-08-31 18:12:22 +0200

    created interface for product + mock data

---
+++ src/ProductData.ts
@@ -0,0 +1,126 @@
+export interface Product {
+  id: number;
+  designer: string;
+  title: string;
+  descripton: string;
+  price: number;
+  category: string;
+  img: string[];
+  size?: string;
+  color?: string;
+}
+
+export const products: Product[] = [
+  {
+    id: 1,
+    designer: "Oscar Stenson",
+    title: "Kånken",
+    descripton: "Generic hipster bag",
+    price: 250,
+    category: "Accessories",
+    img: [
+      "https://www.fotoagent.dk/single_picture/12535/138/custom1/138450752.jpg",
+      "https://www.fotoagent.dk/single_picture/12535/138/mega/138450751.jpg",
+      "https://www.fotoagent.dk/single_picture/12535/138/mega/138450753.jpg",
+    ],
+    color: "Orange",
+  },
+  {
+    id: 2,
+    designer: "Tom Sachs",
+    title: "General purpose shoe",
+    descripton: "A shoe that is made for wearing and can withstand tough conditions",
+    price: 1500,
+    category: "Shoe",
+    img: [
+      "https://sneakernews.com/wp-content/uploads/2022/08/Tom-Sachs-Nike-GPS-Dark-Sulfur-2.jpg?w=1140",
+      "https://sneakernews.com/wp-content/uploads/2022/08/Tom-Sachs-Nike-GPS-Dark-Sulfur-5.jpg?w=1140",
+      "https://sneakernews.com/wp-content/uploads/2022/08/Tom-Sachs-Nike-GPS-Dark-Sulfur-1.jpg?w=1140",
+    ],
+    color: "yellow",
+  },
+  {
+    id: 3,
+    designer: "Carne Bollente",
+    title: "Life is bootie-full",
+    descripton: "A shirt made from wool and linen, printed with people dancing naked.",
+    price: 1900,
+    category: "Shirt",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.692903/carne-bollente-life-is-bootie-full-shirt-all-over.jpeg",
+      "https://02.cdn37.se/ak1/images/2.692910/carne-bollente-life-is-bootie-full-shirt-all-over.png",
+      "https://02.cdn37.se/ak1/images/2.677563/carne-bollente-life-is-bootie-full-shirt-all-over.jpeg",
+    ],
+    color: "White",
+  },
+  {
+    id: 4,
+    designer: "Layered",
+    title: "Poppykalas rug flower field",
+    descripton: "A rug from layered woven by hand. Made from wool and tencel",
+    price: 9900,
+    category: "Rug",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.516099/layered-poppykalas-rug-flower-field.jpeg",
+      "https://02.cdn37.se/ak1/images/2.573995/layered-poppykalas-rug-flower-field.jpeg",
+      "https://02.cdn37.se/ak1/images/2.574010/layered-poppykalas-rug-flower-field.jpeg",
+    ],
+    color: "Pink",
+  },
+  {
+    id: 5,
+    designer: "Birkenstock",
+    title: "Arizona leather black",
+    descripton: "Black leather sandals made for people with tender little hearts. Preferably worn with multicolored socks.",
+    price: 1300,
+    category: "Shoe",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.667048/birkenstock-arizona-oiled-nubuck-leather-black.jpeg",
+      "https://02.cdn37.se/ak1/images/2.667038/birkenstock-arizona-oiled-nubuck-leather-black.jpeg",
+      "https://02.cdn37.se/ak1/images/2.667053/birkenstock-arizona-oiled-nubuck-leather-black.jpeg",
+    ],
+    color: "Black",
+  },
+  {
+    id: 6,
+    designer: "Lemaire",
+    title: "Twisted belted pants",
+    descripton: "Workwear inspired pants, designed with twisted seams alongside the outer leg which provides a fit with a lot of volume.",
+    price: 2300,
+    category: "Pants",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.689628/lemaire-twisted-belted-pants-misty-ivory.jpeg",
+      "https://02.cdn37.se/ak1/images/2.689873/lemaire-twisted-belted-pants-misty-ivory.jpeg",
+      "https://02.cdn37.se/ak1/images/2.689878/lemaire-twisted-belted-pants-misty-ivory.jpeg",
+    ],
+    color: "Ivory",
+  },
+  {
+    id: 7,
+    designer: "Atelier Yuchi",
+    title: "Capri woven blanket",
+    descripton: "Blanket from Atelier Yuchi made from hand woven wool, 138 x 184cm",
+    price: 2400,
+    category: "blanket",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.672628/atelier-yuchi-capri-woven-blanket.jpeg",
+      "https://02.cdn37.se/ak1/images/2.672818/atelier-yuchi-capri-woven-blanket.jpeg",
+      "https://02.cdn37.se/ak1/images/2.672638/atelier-yuchi-capri-woven-blanket.jpeg",
+    ],
+    color: "Multi colored",
+  },
+  {
+    id: 8,
+    designer: "Layered",
+    title: "Modern oriental rug",
+    descripton: "Oriental rug made from 100% wool. Inspired by the Romanian sculptor Constantin Brancusi.",
+    price: 8900,
+    category: "Rug",
+    img: [
+      "https://02.cdn37.se/ak1/images/2.639563/layered-modern-oriental-rug-saffron.jpeg",
+      "https://02.cdn37.se/ak1/images/2.573975/layered-modern-oriental-rug-saffron.jpeg",
+      "https://02.cdn37.se/ak1/images/2.516090/layered-modern-oriental-rug-saffron.jpeg",
+    ],
+    color: "Ivory",
+  },
+];
