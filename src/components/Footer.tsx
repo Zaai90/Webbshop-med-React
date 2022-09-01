fbd3d90facb0ec8f1db5d95259e0d8bae4c32234
4l3x <alexander.auermaa@gmail.com>
2022-09-01 19:11:15 +0200

    Links are now responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import AutorenewOutlinedIcon from "@mui/icons-material/AutorenewOutlined";
 import BoltOutlinedIcon from "@mui/icons-material/BoltOutlined";
 import ForestOutlinedIcon from "@mui/icons-material/ForestOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import RedeemOutlinedIcon from '@mui/icons-material/RedeemOutlined';
+import RedeemOutlinedIcon from "@mui/icons-material/RedeemOutlined";
 import styled from "styled-components";
 
 const FooterStyled = styled.footer``;
@@ -48,21 +48,54 @@ const BenefitContent = styled.div`
 `;
 
 const LinksContainer = styled.div`
-  padding: 40px 15px;
-  background-color: #eee; 
-  display: flex;
-  gap: 4rem;
-  justify-content: center;
+  padding: 40px 20px;
+  max-width: 1000px;
+  margin: 0 auto;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  display: grid;
+  gap: 0.75rem 0;
+  font-size: 10px;
+
+  @media (min-width: 449px) {
+    font-size: 12px;
+  }
 `;
 
 const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
 
+  h4 {
+    width: fit-content;
+    padding-bottom: 5px;
+    border-bottom: 1px solid black;
+  }
+`;
+
+const SocialsContainer = styled.div`
+  width: 100%;
+`;
+
+const SocialsContent = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const LinksWrapper = styled.div`
+  background-color: #eee;
+`;
+
+const GiftContainer = styled.div`
+  display: flex;
+  gap: 0.25rem;
+  align-items: center;
 `;
 
 const Footer = () => {
   return (
     <FooterStyled>
-            <BenefitContainerStyled>
+      <BenefitContainerStyled>
         <BenefitContent>
           <ForestOutlinedIcon />
           <span>Sustainable shopping</span>
@@ -83,39 +116,43 @@ const Footer = () => {
           <span>Lightning fast deliveries</span>
         </BenefitContent>
       </BenefitContainerStyled>
-
-      <LinksContainer>
-        <div>
-          <h4>Contact</h4>
-          <div>Skaraborgsvägen 3</div>
-          <div>506 30 Borås</div>
-          <div>support@tfcstore.dev</div>
-          <div>+46 33-25 54 55</div>
-        </div>
-        <div>
+      <BenefitContainerStyled>
+        <SocialsContainer>
           <h4>Follow us</h4>
-          <div>Facebook</div>
-          <div>Snapchat</div>
-          <div>Instagram</div>
-        </div>
-        <div>
-          <h4>Shopping</h4>
-          <div>Kategori 1</div>
-          <div>Kategori 2</div>
-          <div>Kategori 3</div>
-          <div>Kategori 4</div>
-        </div>
-        <div>
-          <h4>Gift Card</h4>
-          <div>
-          <RedeemOutlinedIcon />
-          <div>Give a gift card to someone!</div>
-          </div>
-        </div>
-      </LinksContainer>
-
+          <SocialsContent>
+            <div>X</div>
+            <div>Z</div>
+            <div>Y</div>
+          </SocialsContent>
+        </SocialsContainer>
+      </BenefitContainerStyled>
+      <LinksWrapper>
+        <LinksContainer>
+          <LinkContainer>
+            <h4>Contact</h4>
+            <div>Skaraborgsvägen 3</div>
+            <div>506 30 Borås</div>
+            <div>support@tfcstore.dev</div>
+            <div>+46 33-25 54 55</div>
+          </LinkContainer>
+          <LinkContainer>
+            <h4>Shopping</h4>
+            <div>Kategori 1</div>
+            <div>Kategori 2</div>
+            <div>Kategori 3</div>
+            <div>Kategori 4</div>
+          </LinkContainer>
+          <LinkContainer>
+            <h4>Gift Card</h4>
+            <GiftContainer>
+              <RedeemOutlinedIcon />
+              <div>Give a gift card to someone!</div>
+            </GiftContainer>
+          </LinkContainer>
+        </LinksContainer>
+      </LinksWrapper>
     </FooterStyled>
   );
-}
+};
 
-export default Footer
+export default Footer;
