a4d70118858f32ab11859d0334c8d0c31256fdb0
4l3x <alexander.auermaa@gmail.com>
2022-09-01 17:46:53 +0200

    Benefit area added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,71 @@ import RedeemOutlinedIcon from '@mui/icons-material/RedeemOutlined';
 import styled from "styled-components";
 
 const FooterStyled = styled.footer``;
+const BenefitContainerStyled = styled.div`
+  padding: 30px 15px;
+  background-color: #f7f7f7;
+  display: flex;
+  gap: 4rem;
+  justify-content: center;
 
+  svg {
+    width: 3rem;
+    height: 3rem;
+  }
+
+  @media (max-width: 768px) {
+    gap: 1rem;
+
+    svg {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+`;
+
+const BenefitContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+
+  span {
+    text-align: center;
+    font-size: 14px;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    span {
+      font-size: 10px;
+    }
+  }
+`;
 const Footer = () => {
   return (
-    <FooterStyled></FooterStyled>
+    <FooterStyled>
+            <BenefitContainerStyled>
+        <BenefitContent>
+          <ForestOutlinedIcon />
+          <span>Sustainable shopping</span>
+        </BenefitContent>
+
+        <BenefitContent>
+          <LockOpenOutlinedIcon />
+          <span>Safe payment options</span>
+        </BenefitContent>
+
+        <BenefitContent>
+          <AutorenewOutlinedIcon />
+          <span>Free returns</span>
+        </BenefitContent>
+
+        <BenefitContent>
+          <BoltOutlinedIcon />
+          <span>Lightning fast deliveries</span>
+        </BenefitContent>
+      </BenefitContainerStyled>
+    </FooterStyled>
   );
 }
 
