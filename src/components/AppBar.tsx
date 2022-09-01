bcff590444fb8ee8935527fb57c98d8ab1fdbdf7
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-01 11:01:58 +0200

    Added icon wrapped in a badge that displays cart.length

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,14 @@
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar as MUIAppBar, IconButton, Toolbar } from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { AppBar as MUIAppBar, IconButton, Toolbar, Badge } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { useCart } from "../contexts/CartContext";
 import AppBarDrawer from "./AppBarDrawer";
 
 const AppBar = () => {
+  const { cart } = useCart();
+
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   function toggleDrawer() {
     setIsDrawerOpen((prev) => !prev);
@@ -12,11 +16,14 @@ const AppBar = () => {
   return (
     <MUIAppBar color="default" position="fixed">
       <Container maxWidth="lg">
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
           <AppBarDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
           <IconButton onClick={toggleDrawer} size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
+          <Badge badgeContent={cart.length} showZero color="primary">
+            <ShoppingCartOutlinedIcon />
+          </Badge>
         </Toolbar>
       </Container>
     </MUIAppBar>
