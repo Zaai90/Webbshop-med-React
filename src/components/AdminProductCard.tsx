1100ff76e65132276dedc560edb8c86a424982d0
osci-the-orca <88oscste@skola.boras.se>
2022-09-01 20:48:54 +0200

    added state for editMode, and some text to show what data is displayed.

--- src/components/AdminProductCard.tsx
+++ src/components/AdminProductCard.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Form from "../components/Form";
 import { useProducts } from "../contexts/ProductContext";
 import { Product } from "../ProductData";
 
@@ -8,11 +9,11 @@ const ProductCard = styled.div`
   display: flex;
   max-width: 760px;
   flex-direction: column;
+  border: 1px solid black;
 `;
 
 const Row = styled.div`
   display: flex;
-  border: 1px solid black;
   gap: 2rem;
   align-items: center;
 `;
@@ -24,11 +25,13 @@ const Image = styled.img`
 const Info = styled.div<{ open: boolean }>`
   display: ${(props) => (props.open ? "flex" : "none")};
   transition: all 2s;
+  flex-direction: column;
 `;
 
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
+  cursor: pointer;
 `;
 
 interface Props {
@@ -37,27 +40,34 @@ interface Props {
 
 const AdminProductCard = ({ product }: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
 
   const { deleteProduct, editProduct } = useProducts();
 
+  useEffect(() => {
+    if (!isOpen) setIsEditMode(false);
+  }, [isOpen]);
+
   return (
-    <ProductCard key={product.id} onClick={() => setIsOpen(!isOpen)}>
-      <Row>
+    <ProductCard key={product.id}>
+      <Row onClick={() => setIsOpen(!isOpen)}>
         <Image src={product.img[0]} />
         <div>{product.id}</div>
         <div>{product.title}</div>
       </Row>
+
       <Info open={isOpen}>
-        <div>{product.designer}</div>
-        <div>{product.description}</div>
-        <div>{product.price}</div>
-        <div>{product.category}</div>
-        <div>{product.color}</div>
-        <div>{product.size}</div>
+        <div>Designer: {product.designer}</div>
+        <div>Description: {product.description}</div>
+        <div>Price: {product.price}</div>
+        <div>Category: {product.category}</div>
+        <div>Color: {product.color}</div>
+        {product.size && <div>Size: {product.size}</div>}
         <ButtonContainer>
-          <Button onClick={() => editProduct}>Edit</Button>
+          <Button onClick={() => setIsEditMode(!isEditMode)}>Edit</Button>
           <Button onClick={() => deleteProduct(product)}>Delete</Button>
         </ButtonContainer>
+        {isEditMode && isOpen && <Form product={product} />}
       </Info>
     </ProductCard>
   );
