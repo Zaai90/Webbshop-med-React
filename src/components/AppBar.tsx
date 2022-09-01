44800fcab070e42977f71be700463ea19402b642
osci-the-orca <88oscste@skola.boras.se>
2022-09-01 16:27:08 +0200

    renamed Products

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,10 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { AppBar as MUIAppBar, IconButton, Toolbar, Badge } from "@mui/material";
+import { AppBar as MUIAppBar, Badge, IconButton, Toolbar } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
 import { useCart } from "../contexts/CartContext";
-import { products } from "../ProductData";
+import { Products } from "../ProductData";
 import AppBarDrawer from "./AppBarDrawer";
 import CartDrawerContent from "./CartDrawerContent";
 import LinksDrawerContent from "./LinksDrawerContent";
@@ -40,8 +40,8 @@ const AppBar = () => {
             <MenuIcon />
           </IconButton>
           {/* addToCart is for testing only, remove later */}
-          <button onClick={() => addToCart(products[0], 1)}>Add dummy</button>
-          <button onClick={() => addToCart(products[1], 1)}>Add dummy item</button>
+          <button onClick={() => addToCart(Products[0], 1)}>Add dummy</button>
+          <button onClick={() => addToCart(Products[1], 1)}>Add dummy item</button>
           <IconButton onClick={toggleCartDrawer} size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <Badge badgeContent={cart.length} showZero color="primary">
               <ShoppingCartOutlinedIcon />
