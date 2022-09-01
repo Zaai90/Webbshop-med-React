3fb7c600dbe3bebf2f8e964bf83d1f316ae6df08
Carl Åfalk <carl.afalk@hotmail.se>
2022-09-01 11:49:00 +0200

    griditem manipulated

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,13 +1,21 @@
-import { Card, CardContent, CardMedia, Grid } from "@mui/material";
+import { Card, CardContent, Grid } from "@mui/material";
 import styled from "styled-components";
 import { Product } from "../ProductData";
 
 const CardImageStyled = styled.div<{ imgUrl: string }>`
   background: url(${(props) => props.imgUrl});
-  background-size: contain;
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  height: 50%;
+  height: 85%;
+`;
+const CardInfoStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  font-size: small;
+  margin-top: 1px;
 `;
 
 interface Props {
@@ -19,7 +27,11 @@ const GridItem = ({ product }: Props) => {
     <Grid item component={Card} minHeight="350px" xs={6} md={4}>
       <CardImageStyled imgUrl={product.img[0]} />
       <CardContent sx={{ height: "100%" }}>
-        <p>{product.designer}</p>
+        <CardInfoStyled>
+          <h5>{product.title}</h5>
+          <p>{product.designer}</p>
+          <p>{product.price}:-</p>
+        </CardInfoStyled>
       </CardContent>
     </Grid>
   );
