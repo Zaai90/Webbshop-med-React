ec4fa3628bcfed1148ebd8bd1e98f2530a7e6506
Tommy Z <85tomzar@skola.boras.se>
2022-09-01 19:29:27 +0200

    CartDrawerContent now in container

--- src/components/CartDrawerContent.tsx
+++ src/components/CartDrawerContent.tsx
@@ -1,14 +1,17 @@
+import { Container } from "@mui/material";
 import { useCart } from "../contexts/CartContext";
 import CartDrawerItem from "./CartDrawerItem";
 
 const CartDrawerContent = () => {
   const { cart } = useCart();
   return (
-    <div style={{ width: "100%" }}>
-      {cart.map((cartItem, index) => (
-        <CartDrawerItem key={index} cartItem={cartItem} />
-      ))}
-    </div>
+    <Container maxWidth="lg">
+      <div style={{ width: "100%" }}>
+        {cart.map((cartItem, index) => (
+          <CartDrawerItem key={index} cartItem={cartItem} />
+        ))}
+      </div>
+    </Container>
   );
 };
 
