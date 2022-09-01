d40f4b5f5032d776d009db63f2b1940c2914670e
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-01 14:50:39 +0200

    created and implemented CartDrawerItem

--- src/components/CartDrawerContent.tsx
+++ src/components/CartDrawerContent.tsx
@@ -1,8 +1,15 @@
 import { useCart } from "../contexts/CartContext";
+import CartDrawerItem from "./CartDrawerItem";
 
 const CartDrawerContent = () => {
   const { cart } = useCart();
-  return <div>{cart.length > 0 ? "You have things in your cart!" : "Cart is empty!"}</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      {cart.map((cartItem) => (
+        <CartDrawerItem cartItem={cartItem} />
+      ))}
+    </div>
+  );
 };
 
 export default CartDrawerContent;
