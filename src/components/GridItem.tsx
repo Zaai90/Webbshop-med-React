36a0837de361ce7b88ac65251582d9caaf64cdbe
4l3x <alexander.auermaa@gmail.com>
2022-09-02 21:45:57 +0200

    QuickView added to gridItems, did some cleanup

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -11,28 +11,27 @@ const CardImageStyled = styled.div<{ imgUrl: string }>`
   background-color: #f7f7f7;
   background-repeat: no-repeat;
   background-position: center;
-  height: 80%;
+  height: 350px;
   width: 100%;
   border-radius: none;
   cursor: pointer;
+  transition: .5s ease-in-out;
+
+  @media (max-width: 768px) {
+    height: 200px;
+  }
 `;
+
 const CardBottomStyled = styled.div`
-  height: 20%;
+  position: relative;
+  z-index: 100;
+  background-color: white;
   display: flex;
   justify-content: center;
   flex-direction: row;
   text-align: center;
   font-size: small;
   position: relative;
-  @media (min-width: 450px) {
-    margin: auto 0;
-  }
-  @media (max-width: 450px) {
-    font-size: 10px !important;
-  }
-  @media (max-height: 600px) {
-    font-size: 10px !important;
-  }
 `;
 
 const IconButtonStyled = styled(IconButton)`
@@ -42,24 +41,40 @@ const IconButtonStyled = styled(IconButton)`
   color: black !important;
 `;
 
+
+const QuickView = styled.span`
+    position: absolute;
+    bottom: 0px;
+    left: 0;
+    right: 0;
+    padding: 10px;
+    background: rgba(0,0,0,.5);
+    z-index: 4;
+    transition: .5s ease all;
+    text-align: center;
+
+    a {
+      text-decoration: none;
+      font-size: 12px;
+      color: white;
+      text-shadow: 2px 2px 4px black;
+    }
+`
+
 const CardStyled = styled(Card)`
   box-shadow: none !important;
   border-radius: none;
-  height: 35vh;
-
-  @media (min-width: 580px) {
-    height: 50vh;
-  }
-  @media (min-width: 700px) {
-    height: 40vh;
-  }
-  @media (min-width: 1050px) {
-    height: 50vh;
-  }
-  @media (max-height: 600px) {
-    height: 60vh;
+  position: relative;
+  &:hover {
+    ${QuickView} {
+      transform: translateY(-125%);
+   }
+   ${CardImageStyled} {
+    scale: 1.05;
+   }
   }
 `;
+
 
 interface Props {
   product: Product;
@@ -73,6 +88,9 @@ const GridItem = ({ product }: Props) => {
       <NavLink to={`../product/${product.id}`}>
         <CardImageStyled imgUrl={product.img[0]} />
       </NavLink>
+      <QuickView>
+        <a href="#">Quick View</a>
+      </QuickView>
       <CardBottomStyled>
         <div>
           <h5>{product.title}</h5>
