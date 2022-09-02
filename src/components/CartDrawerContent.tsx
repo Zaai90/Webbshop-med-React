8f08efac51d2fa79bc809ecf072a772405c71cd8
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-02 21:27:59 +0200

    cart styling + NavLink to checkout

--- src/components/CartDrawerContent.tsx
+++ src/components/CartDrawerContent.tsx
@@ -1,4 +1,5 @@
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { useCart } from "../contexts/CartContext";
 import CartDrawerItem from "./CartDrawerItem";
@@ -22,22 +23,33 @@ const Total = styled.div`
   padding: 0.5rem;
 `;
 
-const CartDrawerContent = () => {
+interface Props {
+  toggleDrawer: () => void;
+}
+
+const CartDrawerContent = ({ toggleDrawer }: Props) => {
   const { cart, totalAmount } = useCart();
   return (
     <>
-      <Wrapper>
-        <Title>CART</Title>
-        {cart.length == 0 && <div>Cart is empty</div>}
-        {cart.map((cartItem) => (
-          <CartDrawerItem key={cartItem.product.id} cartItem={cartItem} />
-        ))}
-      </Wrapper>
-      <Total>
-        <span>Total: </span>
-        <span>{totalAmount}</span>
-      </Total>
-      <button>CHECKOUT BUTTON HERE</button>
+      {cart.length == 0 ? (
+        <div>Cart is empty</div>
+      ) : (
+        <>
+          <Wrapper>
+            <Title>CART</Title>
+            {cart.map((cartItem) => (
+              <CartDrawerItem key={cartItem.product.id} cartItem={cartItem} />
+            ))}
+          </Wrapper>
+          <Total>
+            <span style={{ fontWeight: 500 }}>Total: </span>
+            <span>{totalAmount} SEK</span>
+          </Total>
+          <NavLink onClick={toggleDrawer} to="checkout">
+            Checkout
+          </NavLink>
+        </>
+      )}
     </>
   );
 };
