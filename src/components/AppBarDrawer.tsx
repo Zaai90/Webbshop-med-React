a6146cf4856b90d98b1e1a9648c5186c8ba81b78
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-03 11:04:39 +0200

    cart drawer now has responsive width

--- src/components/AppBarDrawer.tsx
+++ src/components/AppBarDrawer.tsx
@@ -1,11 +1,13 @@
 import * as Icon from "@mui/icons-material/";
 import { Box, Drawer } from "@mui/material";
 import { ReactNode } from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 
-const AppBarDrawerStyled = styled(Drawer)<{ anchor: string }>`
+const AppBarDrawerStyled = styled(Drawer)<{ anchor: string; isPhoneScreen: boolean }>`
   .MuiPaper-root {
     overflow: ${(props) => (props.anchor === "left" ? "visible" : "scroll")};
+    ${(props) => props.anchor === "right" && props.isPhoneScreen && "width: 100%;"}
   }
 `;
 
@@ -35,10 +37,23 @@ interface Props {
   anchor: "left" | "right";
 }
 const AppBarDrawer = ({ toggleDrawer, isOpen, children, anchor }: Props) => {
+  const isPhoneScreen = useMediaQuery({ query: "(max-width:768px)" });
+
+  const drawerWidth = (anchor: "left" | "right") => {
+    if (anchor === "right") {
+      if (isPhoneScreen) {
+        return "100%";
+      } else {
+        return "350px";
+      }
+    } else {
+      return "250px";
+    }
+  };
   return (
-    <AppBarDrawerStyled anchor={anchor} open={isOpen} onClose={toggleDrawer}>
+    <AppBarDrawerStyled anchor={anchor} open={isOpen} onClose={toggleDrawer} isPhoneScreen={isPhoneScreen}>
       {anchor === "left" && <CloseButtonStyled onClick={toggleDrawer} anchor={anchor} />}
-      <AppBarBoxStyled width={anchor === "left" ? "250px" : "360px"}>{children}</AppBarBoxStyled>
+      <AppBarBoxStyled width={drawerWidth(anchor)}>{children}</AppBarBoxStyled>
     </AppBarDrawerStyled>
   );
 };
