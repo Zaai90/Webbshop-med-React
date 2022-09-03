a73ed756d267760e90d9dfdc46548d54fa36a7d2
4l3x <alexander.auermaa@gmail.com>
2022-09-03 23:39:04 +0200

    Swiper added to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Container } from "@mui/material";
 import styled from "styled-components";
 import MainContent from "../components/MainContent";
 import { useProducts } from "../contexts/ProductContext";
+
+// Import Swiper React components
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import { useState } from "react";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const TopContent = styled.div`
   padding: 60px;
@@ -44,20 +55,70 @@ const CategoryBubbleContainer = styled.div`
   }
 `;
 
-const MiddleContent = styled.div`
-  height: 400px;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
+const SwiperContent = styled.div`
   align-items: center;
   margin: 0 auto;
+  text-align: center;
+
+  .swiper {
+    /* position: relative !important; */
+  }
+  .swiper-button-next {
+    right: 5px;
+  }
+  .swiper-button-prev {
+    left: 0px;
+  }
+  .swiper-button-next,
+  .swiper-button-prev {
+    color: white;
+    background-color: rgba(0, 0, 0, 0.15);
+    padding: 30px;
+    top: 45%;
+  }
+
+  @media screen and (max-width: 360px) {
+    .swiper-container {
+      width: 300px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .swiper-container {
+      width: 768px;
+    }
+  }
+  @media only screen and (max-width: 1200px) {
+    .swiper-button-next,
+    .swiper-button-prev {
+      display: none;
+    }
+  }
+`;
+
+const SliderImage = styled.img`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+`;
+
+const FavContainer = styled.div`
+  .hearted {
+    color: #ff5cbb;
+    transition: 1s ease all;
+  }
 `;
 
 const Home = () => {
+  const [isHearted, setHearted] = useState(false);
   const { products } = useProducts();
+
+  const toggleHearted = () => {
+    setHearted(!isHearted);
+  };
   return (
     <>
-      <MiddleContent></MiddleContent>
+      <SwiperContent></SwiperContent>
       <MainContent>
         <Container>
           <TopContent>
@@ -78,9 +139,51 @@ const Home = () => {
               <div>Textiles</div>
             </CategoryBubbleContainer>
           </TopContent>
-          <MiddleContent>
-            <div>Slider goes here</div>
-          </MiddleContent>
+          <SwiperContent>
+            <h1>Slider goes here</h1>
+            <Swiper
+              loop={true}
+              spaceBetween={15}
+              centeredSlides={true}
+              grabCursor={true}
+              slidesPerView={3}
+              navigation={true}
+              modules={[Navigation]}
+              breakpoints={{
+                // when window width is >= 360px
+                360: {
+                  width: 300,
+                  slidesPerView: 1,
+                },
+                // when window width is >= 768px
+                768: {
+                  width: 1100,
+                  slidesPerView: 3,
+                },
+              }}
+            >
+              {products.map((product) => (
+                <SwiperSlide>
+                  <a href={"product/" + product.id}>
+                    <SliderImage src={product.img[0]} />
+                  </a>
+                  {/* TODO: Clean up */}
+                  <div style={{ display: "flex" }}>
+                    <div style={{ margin: "0 auto" }}>{product.title}</div>
+                    <FavContainer>
+                      {/* TODO: Send product to wishlist */}
+                      <div className={isHearted ? "hearted" : undefined} onClick={toggleHearted}>
+                        {/* TODO: Check state on each instead of all */}
+                        {isHearted && <FavoriteIcon />}
+                        {!isHearted && <FavoriteBorderIcon />}
+                      </div>
+                    </FavContainer>
+                  </div>
+                  <div>{product.price} SEK</div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </SwiperContent>
         </Container>
       </MainContent>
     </>
