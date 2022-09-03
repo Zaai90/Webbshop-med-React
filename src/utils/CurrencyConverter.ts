304b09b7edb17144c78b45388e7c3d24576be38d
Zaai90 <erik.grune@gmail.com>
2022-09-03 23:33:18 +0200

    Remove math.round

--- src/utils/CurrencyConverter.ts
+++ src/utils/CurrencyConverter.ts
@@ -3,13 +3,13 @@ import { Currency } from "../contexts/CurrencyContext";
 export const currencyConverter = (value: number, currency: Currency) => {
   switch (currency) {
     case "USD":
-      return Math.round(value * 0.09270383);
+      return value * 0.09270383;
     case "EUR":
-      return Math.round(value * 0.09313892723);
+      return value * 0.09313892723;
     case "GBP":
-      return Math.round(value * 0.08057754884);
+      return value * 0.08057754884;
     case "SEK":
-      return Math.round(value);
+      return value;
     default:
       return Math.round(value);
   }
