e7b4c4f645228b32f29d1db7f28e299f35a48d9e
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-06 16:08:39 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,29 +9,6 @@ import FavoritesProvider from "./contexts/FavoriteContext";
 import ProductProvider from "./contexts/ProductContext";
 import theme from "./utils/Theme";
 
-// declare module "@mui/material/styles" {
-//   interface BreakpointOverrides {
-//     tablet: true;
-//   }
-// }
-
-// const theme = createTheme({
-//   breakpoints: {
-//     keys: ["xs", "sm", "tablet", "md", "lg", "xl"],
-//     values: {
-//       xs: 0,
-//       sm: 600,
-//       tablet: 768,
-//       md: 900,
-//       lg: 1200,
-//       xl: 1536,
-//     },
-//   },
-//   typography: {
-//     fontFamily: "'Poppins','Roboto', 'Helvetica', sans-serif",
-//   },
-// });
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
