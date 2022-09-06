ce7ac97e7752adfe9ce945b44c11d9abde829e9e
Zaai90 <erik.grune@gmail.com>
2022-09-06 22:31:08 +0200

    fix styling

--- src/components/ImagePresenter.tsx
+++ src/components/ImagePresenter.tsx
@@ -4,22 +4,31 @@ import { Product } from "../ProductData";
 
 const ImagePresenterStyled = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column-reverse;
   align-items: center;
   justify-content: center;
   gap: 1rem;
   padding: 1rem;
   width: 100%;
   height: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 const ImageContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   gap: 1rem;
   height: 100%;
+  padding: 0 2rem;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const SelectedImageContainer = styled.div`
@@ -27,28 +36,32 @@ const SelectedImageContainer = styled.div`
 `;
 
 const SelectImageShow = styled.img`
-  max-width: 250px;
-  max-height: 250px;
+  max-width: 400px;
+  max-height: 400px;
   object-fit: contain;
-  border: 1px solid black;
-
-  /* @media (min-width: 768px) {
-    max-width: 480px;
-    max-height: 480px;
+  padding: 1rem;
+  /* 
+  @media (min-width: 600px) {
+    max-width: 500px;
+    max-height: 500px;
   } */
+
+  @media (min-width: 768px) {
+    max-width: 600px;
+    max-height: 600px;
+  }
 `;
 
 const ImageStyled = styled.img`
-  max-width: 100px;
-  max-height: 100px;
+  max-width: 180px;
+  max-height: 180px;
   object-fit: contain;
   cursor: pointer;
-  border: 1px solid black;
 
-  /* @media (min-width: 768px) {
-    max-width: 150px;
-    max-height: 150px;
-  } */
+  @media (min-width: 768px) {
+    max-width: 200px;
+    max-height: 200px;
+  }
 `;
 
 interface ImagePresenterProps {
@@ -57,9 +70,32 @@ interface ImagePresenterProps {
 
 const ImagePresenter = ({ product }: ImagePresenterProps) => {
   const [selectedImg, setSelectedImg] = useState<string>(product.img[0]);
-  const images: JSX.Element[] = product.img.map((img: string, index: number) => {
-    return <ImageStyled draggable="false" key={index} src={img} alt={product.title} onMouseEnter={() => setSelectedImg(img)} />;
+  const [hover, setHover] = useState(false);
+
+  let images: JSX.Element[] = [];
+  images = product.img.map((img: string, index: number) => {
+    return img !== selectedImg ? (
+      <ImageStyled
+        draggable="false"
+        key={index}
+        src={img}
+        alt={product.title}
+        onMouseEnter={() => {
+          if (!hover) {
+            setHover(true);
+            setSelectedImg(img);
+            console.log("spam");
+          }
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+      />
+    ) : (
+      <></>
+    );
   });
+
   return (
     <ImagePresenterStyled>
       <ImageContainer>{images}</ImageContainer>
