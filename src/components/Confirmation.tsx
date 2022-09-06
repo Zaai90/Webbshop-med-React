15545c28d419c5338c3732b790972c5f22419c3e
4l3x <alexander.auermaa@gmail.com>
2022-09-06 22:35:32 +0200

    Next button moved to new components

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -1,5 +1,19 @@
-const Confirmation = () => {
-  return <div>Confirmation</div>;
+import { Button } from "@mui/material";
+
+interface Props {
+  handleSubmit: () => void;
+}
+
+const Confirmation = ({ handleSubmit }: Props) => {
+  return (
+    <div>
+      <div>Confirmation</div>
+      {/* TODO: Clear cart */}
+      <Button color="primary" variant="contained" fullWidth type="submit" onClick={handleSubmit}>
+        Finish order
+      </Button>
+    </div>
+  );
 };
 
 export default Confirmation;
--- src/components/PaymentOptions.tsx
+++ src/components/PaymentOptions.tsx
@@ -1,5 +1,18 @@
-const PaymentOptions = () => {
-  return <div>PaymentOptions</div>;
+import { Button } from "@mui/material";
+
+interface Props {
+  handleSubmit: () => void;
+}
+
+const PaymentOptions = ({ handleSubmit }: Props) => {
+  return (
+    <div>
+      <div>PaymentOptions</div>
+      <Button color="primary" variant="contained" fullWidth type="submit" onClick={handleSubmit}>
+        Next step
+      </Button>
+    </div>
+  );
 };
 
 export default PaymentOptions;
