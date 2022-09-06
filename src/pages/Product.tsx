9e5939c01bf92f298bb5043eb1a7c22b34ff87a0
Zaai90 <erik.grune@gmail.com>
2022-09-06 17:18:35 +0200

    add mediaqueries

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import ImagePresenter from "../components/ImagePresenter";
 import MainContent from "../components/MainContent";
-import { Currency, useCurrency } from "../contexts/CurrencyContext";
+import { useCurrency } from "../contexts/CurrencyContext";
 import { useProducts } from "../contexts/ProductContext";
 
 const ContainerStyled = styled.div`
   display: flex;
   gap: 1rem;
   justify-content: center;
+  flex-direction: column;
   min-width: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 const InfoContainer = styled.div`
@@ -21,75 +26,57 @@ const InfoContainer = styled.div`
   padding: 1rem;
 `;
 
-const ImageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  height: 100%;
-`;
-
 const TitleStyled = styled.h1`
-  font-size: 1.5rem;
+  font-size: 1rem;
+  font-weight: 500;
+  color: grey;
+
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+    color: black;
+  }
 `;
 
 const DesignerStyled = styled.h2`
-  font-size: 0.8rem;
-  color: grey;
+  font-weight: 500;
+  font-size: 1.1rem;
+
+  @media (min-width: 768px) {
+    font-size: 0.8rem;
+    color: grey;
+  }
 `;
 
 const PriceStyled = styled.p`
-  font-size: 1.5rem;
-  margin-top: -1.05rem;
+  font-size: 1.1rem;
   font-weight: 500;
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+    margin-top: -1.05rem;
+    font-weight: 500;
+  }
 `;
 
 const DescriptionStyled = styled.p`
-  font-size: 1.5rem;
-  font-weight: 400;
-`;
-
-const ImageStyled = styled.img`
-  max-width: 150px;
-  max-height: 150px;
-  object-fit: contain;
-  cursor: pointer;
-  border: 1px solid black;
-`;
-
-const SelectedImageContainer = styled.div`
-  display: flex;
-`;
-
-const SelectImageShow = styled.img`
-  max-width: 400px;
-  max-height: 400px;
-  object-fit: contain;
-  border: 1px solid black;
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
 `;
 
 const ProductPage = () => {
   const { id } = useParams();
   const { products } = useProducts();
   const product = products.find((p) => p.id === Number(id)) ?? products[0];
-  const [selectedImg, setSelectedImg] = useState<string>(product.img[0]);
 
-  const { convertToCurrencyValue, changeCurrency } = useCurrency();
-
-  const images: JSX.Element[] = product.img.map((img: string, index: number) => {
-    return <ImageStyled draggable="false" key={index} src={img} alt={product.title} onMouseEnter={() => setSelectedImg(img)} />;
-  });
+  const { convertToCurrencyValue } = useCurrency();
 
   return (
     <MainContent>
       <ContainerStyled>
-        <ImageContainer>{images}</ImageContainer>
-        <SelectedImageContainer>
-          <SelectImageShow draggable="false" src={selectedImg} />
-        </SelectedImageContainer>
+        <ImagePresenter product={product} />
         <InfoContainer>
-          <div style={{ display: "flex", flexDirection: "row", gap: "12rem", padding: 0, margin: 0 }}>
+          <div style={{ display: "flex", flexDirection: "row", gap: "5rem", padding: 0, margin: 0 }}>
             <DesignerStyled>{product.designer}</DesignerStyled>
             <PriceStyled>{convertToCurrencyValue(product.price)}</PriceStyled>
           </div>
