21a2e2d0c500b2b6d0fea8c6358dbf7f786cde71
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-07 21:27:43 +0200

    broke out AppBarLinks and Logo

---
+++ src/components/AppBar/AppBarLinks.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderLinks = styled(Box)`
+  display: flex;
+  font-size: 18px;
+  line-height: normal;
+  margin-right: auto;
+  margin-left: 2rem;
+
+  a {
+    padding: 15px;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #ffffff;
+    transition: 0.5s ease all;
+  }
+
+  a:hover {
+    color: #8d8d8d;
+  }
+`;
+
+interface Props {
+  pages: string[];
+}
+
+const AppBarLinks = ({ pages }: Props) => {
+  return (
+    <HeaderLinks>
+      {pages.map((page) => (
+        <NavLink to={page}>
+          <Button size="large">
+            <Typography variant="h6" color={(theme) => theme.palette.common.white} textTransform="uppercase">
+              {page}
+            </Typography>
+          </Button>
+        </NavLink>
+      ))}
+    </HeaderLinks>
+  );
+};
+
+export default AppBarLinks;
