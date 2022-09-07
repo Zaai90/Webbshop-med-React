36e58b1981771bca26b513f30f842e97eabbeb09
4l3x <alexander.auermaa@gmail.com>
2022-09-07 15:13:21 +0200

    Options added

--- src/components/PaymentOptions.tsx
+++ src/components/PaymentOptions.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@mui/material";
+import styled from "styled-components";
 
 interface Props {
   handleSubmit: () => void;
 }
 
+const PaymentOption = styled.div``;
+
 const PaymentOptions = ({ handleSubmit }: Props) => {
   return (
     <div>
       <div>PaymentOptions</div>
+      <PaymentOption>Trade</PaymentOption>
+      <PaymentOption>Google Pay</PaymentOption>
+      <PaymentOption>VISA</PaymentOption>
+      <PaymentOption>PayPal</PaymentOption>
+      <PaymentOption>MasterCard</PaymentOption>
+      <PaymentOption>Swish</PaymentOption>
+      <PaymentOption>Klarna</PaymentOption>
+      <PaymentOption>Apple Pay</PaymentOption>
       <Button color="primary" variant="contained" fullWidth type="submit" onClick={handleSubmit}>
         Next step
       </Button>
