63e9be773062834403b21251a594ca01b8996f29
4l3x-suvnet <alexander.auermaa@gmail.com>
2022-09-08 04:01:58 +0200

    Checkout is now inaccessible if cart is empty

---
+++ src/components/Checkout/EmptyCart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EmptyCart = () => {
+  return (
+      <div>Cart is empty.</div>
+    // TODO: Add more content, suggestive things the user can buy blabla
+  )
+}
+
+export default EmptyCart
