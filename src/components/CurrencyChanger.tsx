2c44503db6015ff034ea6176a26a71c6bc84cc90
Zaai90 <erik.grune@gmail.com>
2022-09-08 13:09:17 +0200

    use currencyContext

--- src/components/CurrencyChanger.tsx
+++ src/components/CurrencyChanger.tsx
@@ -1,10 +1,9 @@
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
-import { useEffect, useState } from "react";
 import { Currency, useCurrency } from "../contexts/CurrencyContext";
 
 const CurrencyChanger = () => {
-  const [currency, setCurrency] = useState(Currency.SEK);
-  const { changeCurrency } = useCurrency();
+  const { currency, changeCurrency } = useCurrency();
+  const actualCurrency: Currency = currency;
 
   const menuItems = Object.values(Currency).map((currency) => (
     <MenuItem key={currency} value={currency}>
@@ -12,20 +11,15 @@ const CurrencyChanger = () => {
     </MenuItem>
   ));
 
-  useEffect(() => {
-    changeCurrency(currency[0] as Currency);
-  }, [currency]);
-
   function handleChange(event: SelectChangeEvent<{ value: Currency }>) {
     const choosenCurrency: Currency = event.target.value as Currency;
     changeCurrency(choosenCurrency);
-    setCurrency(choosenCurrency);
   }
 
   return (
     <FormControl fullWidth>
       <InputLabel id="currency-select-label">Currency</InputLabel>
-      <Select labelId="currency-select-label" id="currency-select" label="Currency" onChange={handleChange}>
+      <Select labelId="currency-select-label" id="currency-select" label="Currency" defaultValue={{ value: actualCurrency }} onChange={handleChange}>
         {menuItems}
       </Select>
     </FormControl>
