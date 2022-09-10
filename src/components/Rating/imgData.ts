1a5aaba8f01739592bb555e3890837a9cd20d666
4l3x <alexander.auermaa@gmail.com>
2022-09-10 02:42:07 +0200

    Array of ratings added

---
+++ src/components/Rating/imgData.ts
@@ -0,0 +1,30 @@
+import amazing from "./img/amazing.webp";
+import angry from "./img/angry.webp";
+import disappointed from "./img/disappointed.webp";
+import fair from "./img/fair.webp";
+import good from "./img/good.webp";
+
+const imgData = [
+  {
+    img: angry,
+    title: "Angry",
+  },
+  {
+      img: disappointed,
+      title: "Disappointed",
+  },
+  {
+      img: fair,
+      title: "Fair",
+    },
+    {
+        img: good,
+        title: "Good",
+    },
+    {
+      img: amazing,
+      title: "Amazing",
+    },
+];
+
+export default imgData;
