0d0406d4b93ac9cf647f74cf8f666706ced2d4d8
Zaai90 <erik.grune@gmail.com>
2022-09-11 02:29:12 +0200

    add functionality for ReadMore

--- src/components/ReadMore.tsx
+++ src/components/ReadMore.tsx
@@ -1,9 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+
 interface Props {
   children: string;
 }
 
 const ReadMore = ({ children }: Props) => {
-  return <div>ReadMore</div>;
+  const [isReadMore, setIsReadMore] = useState(children.length > 100);
+
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  return (
+    <Typography>
+      {isReadMore ? children.slice(0, 100) : children}
+      <Box component="span" onClick={toggleReadMore}>
+        {isReadMore ? "...läs mer" : " visa mindre"}
+      </Box>
+    </Typography>
+  );
 };
 
 export default ReadMore;
