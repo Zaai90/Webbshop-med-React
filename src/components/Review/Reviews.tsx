5b50df08e7966fd835944c3159c85c7b3cc9ca80
4l3x <alexander.auermaa@gmail.com>
2022-09-11 02:32:51 +0200

    Reviews for specific product added

--- src/components/Review/Reviews.tsx
+++ src/components/Review/Reviews.tsx
@@ -1,5 +1,33 @@
-const Reviews = () => {
-  return <div>Reviews</div>;
+import { Box, Container, Typography } from "@mui/material";
+import { Product } from "../../ProductData";
+import ratingData from "./ratingData";
+import { ReviewData } from "./reviewData";
+
+interface Props {
+  product: Product;
+}
+
+const Reviews = ({ product }: Props) => {
+  return (
+    <Container sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+      {ReviewData.filter((review) => review.productId === product.id).map((review) => (
+        <Box>
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <img src={ratingData[review.rating].img} alt={ratingData[review.rating].title} loading="lazy" width={42} />
+            </Box>
+            <Box>
+              <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+                <Typography variant="h6">{review.name}</Typography>
+                <Typography variant="body2">{review.createdAt}</Typography>
+              </Box>
+              <Typography variant="body1">{review.review}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      ))}
+    </Container>
+  );
 };
 
 export default Reviews;
