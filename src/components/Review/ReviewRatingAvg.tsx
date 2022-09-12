ae929bdfa675e9d3347b3f66c6b955bebaef7bbf
4l3x <alexander.auermaa@gmail.com>
2022-09-12 13:19:45 +0200

    avg review rating for x product is now shown in component

--- src/components/Review/ReviewRatingAvg.tsx
+++ src/components/Review/ReviewRatingAvg.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import { Product } from "../../ProductData";
+import ratingData from "./ratingData";
+import { ReviewData } from "./reviewData";
 
-const ReviewRatingAvg = () => {
-  return (
-    <div>ReviewRatingAvg</div>
-  )
+interface Props {
+  product: Product;
 }
 
-export default ReviewRatingAvg
+const ReviewRatingAvg = ({ product }: Props) => {
+  function calcAvgRating(): number {
+    let total = 0;
+    const res = ReviewData.filter((review) => review.productId === product.id).map(
+      (review) => (total += Number(ratingData.indexOf(ratingData[review.rating])))
+    );
+
+    return Number((total / res.length).toFixed());
+  }
+
+  return (
+    <Container sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+      <Box>
+        <img draggable="false" width={32} src={ratingData[calcAvgRating()].img} />
+      </Box>
+      <Typography variant="body2">
+        Users rate this as... <span style={{ fontWeight: 700, textDecoration: "underline" }}>{ratingData[calcAvgRating()].title}</span>
+      </Typography>
+    </Container>
+  );
+};
+
+export default ReviewRatingAvg;
