0c3b956b480caf2eecaaefa3fe702728f1716ba3
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-12 09:43:42 +0200

    finished up quickview drawer

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -111,8 +111,8 @@ const SimpleImageSliderContainer = styled.div`
   display: flex;
   align-items: center;
   padding: 0.5rem;
-  height: 150px !important;
-  width: 150px !important;
+  min-width: 150px !important;
+  min-height: 150px !important;
   position: relative !important;
   .rsis-container div {
     background-position: center center !important;
@@ -122,6 +122,13 @@ const SimpleImageSliderContainer = styled.div`
     filter: invert(100%);
     box-shadow: none !important;
   }
+`;
+
+const DescriptionBox = styled(Typography)`
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 `;
 
 interface Props {
@@ -216,19 +223,40 @@ const GridItem = ({ product }: Props) => {
         <QuickViewModal product={product} size={size} handleChange={handleChange} toggleModal={setIsModalOpen} />
       </Modal>
       <Drawer anchor="bottom" open={isQuickViewDrawerOpen} onClose={() => setIsQuickViewDrawerOpen(false)}>
-        <Container maxWidth="lg" fixed sx={{ display: "flex" }}>
+        <Container maxWidth="lg" fixed sx={{ display: "flex", position: "relative" }}>
+          <IconButton sx={{ position: "absolute", top: 0, right: "1rem" }} onClick={() => setIsQuickViewDrawerOpen((prev) => !prev)}>
+            <Icon.Close />
+          </IconButton>
           <SimpleImageSliderContainer>
             <SimpleImageSlider width={"80%"} height={"80%"} navSize={30} images={product.img} showBullets={true} showNavs={true} navMargin={-10} />
           </SimpleImageSliderContainer>
           <Box sx={{ display: "flex", flexDirection: "column", padding: ".5rem", flexGrow: "1" }}>
             <Typography color={"rgb(159, 159, 159)"}>{product.designer}</Typography>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "3rem" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: ".5rem" }}>
               <Typography fontSize={".8rem"}>{product.title}</Typography>
               <Typography fontSize={".8rem"} fontWeight="700">
                 {convertToCurrencyValue(product.price)}
               </Typography>
             </div>
-            <Typography>{product.title}</Typography>
+            <DescriptionBox fontSize={".8rem"}>{product.description}</DescriptionBox>
+            <Box sx={{ marginTop: ".5rem", display: "flex", flexDirection: "row-reverse" }}>
+              <IconButton onClick={() => handleAdd()}>
+                <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 500 }} title={"Add to cart"} placement="left" arrow>
+                  <Icon.AddShoppingCart />
+                </Tooltip>
+              </IconButton>
+              <IconButton onClick={toggleFavorite}>
+                <Tooltip
+                  TransitionComponent={Fade}
+                  TransitionProps={{ timeout: 500 }}
+                  title={isFavorite ? "Remove from wishlist" : "Add to wishlist"}
+                  placement="left"
+                  arrow
+                >
+                  {isFavorite ? <FavoriteIcon color={"secondary"} /> : <FavoriteBorderIcon color={"disabled"} />}
+                </Tooltip>
+              </IconButton>
+            </Box>
           </Box>
         </Container>
       </Drawer>
