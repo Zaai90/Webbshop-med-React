cda0b41b6d8614fb376f2bffd8c842ac2408c0da
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-12 10:34:21 +0200

    belongs to last commit

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,18 +1,17 @@
 import * as Icon from "@mui/icons-material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { Card, Drawer, Fade, IconButton, Modal, SelectChangeEvent, Tooltip, Typography, useMediaQuery } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import { Card, Drawer, Fade, IconButton, Modal, SelectChangeEvent, Tooltip, useMediaQuery } from "@mui/material";
 import { useSnackbar } from "notistack";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import SimpleImageSlider from "react-simple-image-slider";
 import styled from "styled-components";
 import { useCart } from "../contexts/CartContext";
 import { useCurrency } from "../contexts/CurrencyContext";
 import { useFavorites } from "../contexts/FavoriteContext";
 import { Product } from "../ProductData";
 import theme from "../utils/Theme";
+import QuickViewDrawer from "./Drawers/QuickViewDrawer";
 import AddProductSnackbar from "./Modals/AddProductSnackbar";
 import QuickViewModal from "./Modals/QuickViewModal";
 
@@ -107,30 +106,6 @@ const QuickViewButtonStyled = styled.div`
   }
 `;
 
-const SimpleImageSliderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  min-width: 150px !important;
-  min-height: 150px !important;
-  position: relative !important;
-  .rsis-container div {
-    background-position: center center !important;
-    background-size: contain !important;
-  }
-  & button {
-    filter: invert(100%);
-    box-shadow: none !important;
-  }
-`;
-
-const DescriptionBox = styled(Typography)`
-  display: -webkit-box;
-  overflow: hidden;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-`;
-
 interface Props {
   product: Product;
 }
@@ -223,42 +198,7 @@ const GridItem = ({ product }: Props) => {
         <QuickViewModal product={product} size={size} handleChange={handleChange} toggleModal={setIsModalOpen} />
       </Modal>
       <Drawer anchor="bottom" open={isQuickViewDrawerOpen} onClose={() => setIsQuickViewDrawerOpen(false)}>
-        <Container maxWidth="lg" fixed sx={{ display: "flex", position: "relative" }}>
-          <IconButton sx={{ position: "absolute", top: 0, right: "1rem" }} onClick={() => setIsQuickViewDrawerOpen((prev) => !prev)}>
-            <Icon.Close />
-          </IconButton>
-          <SimpleImageSliderContainer>
-            <SimpleImageSlider width={"80%"} height={"80%"} navSize={30} images={product.img} showBullets={true} showNavs={true} navMargin={-10} />
-          </SimpleImageSliderContainer>
-          <Box sx={{ display: "flex", flexDirection: "column", padding: ".5rem", flexGrow: "1" }}>
-            <Typography color={"rgb(159, 159, 159)"}>{product.designer}</Typography>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: ".5rem" }}>
-              <Typography fontSize={".8rem"}>{product.title}</Typography>
-              <Typography fontSize={".8rem"} fontWeight="700">
-                {convertToCurrencyValue(product.price)}
-              </Typography>
-            </div>
-            <DescriptionBox fontSize={".8rem"}>{product.description}</DescriptionBox>
-            <Box sx={{ marginTop: ".5rem", display: "flex", flexDirection: "row-reverse" }}>
-              <IconButton onClick={() => handleAdd()}>
-                <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 500 }} title={"Add to cart"} placement="left" arrow>
-                  <Icon.AddShoppingCart />
-                </Tooltip>
-              </IconButton>
-              <IconButton onClick={toggleFavorite}>
-                <Tooltip
-                  TransitionComponent={Fade}
-                  TransitionProps={{ timeout: 500 }}
-                  title={isFavorite ? "Remove from wishlist" : "Add to wishlist"}
-                  placement="left"
-                  arrow
-                >
-                  {isFavorite ? <FavoriteIcon color={"secondary"} /> : <FavoriteBorderIcon color={"disabled"} />}
-                </Tooltip>
-              </IconButton>
-            </Box>
-          </Box>
-        </Container>
+        <QuickViewDrawer product={product} toggleDrawer={setIsQuickViewDrawerOpen} handleAdd={handleAdd} />
       </Drawer>
     </>
   );
