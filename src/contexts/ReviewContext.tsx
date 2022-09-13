f778b1303267de36eba90e804666c13fbccc8426
4l3x <alexander.auermaa@gmail.com>
2022-09-13 20:54:06 +0200

    Review context added

---
+++ src/contexts/ReviewContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useContext } from "react";
+import ratingData from "../components/Review/ratingData";
+import { ReviewData } from "../components/Review/reviewData";
+import { useLocalStorage } from "../hooks/localStorage";
+import { ReviewModel } from "../models/ReviewModel";
+import { Product } from "../ProductData";
+
+interface ReviewContextValue {
+  reviews: ReviewModel[];
+    addReview: (review: ReviewModel) => void;
+    calcRating: (product: Product) => number;
+}
+
+const ReviewContext = createContext<ReviewContextValue>({
+  reviews: [],
+    addReview: () => { },
+    calcRating: () => 0,
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+function ReviewProvider({ children }: Props) {
+  const [reviews, setReviews] = useLocalStorage<ReviewModel[]>("reviews", ReviewData);
+
+  const addReview = (review: ReviewModel) => {
+    const reviewsCopy = [...reviews, { ...review, id: 14 }];
+
+    setReviews(reviewsCopy);
+  };
+
+  const calcRating = (product: Product) => {
+    let total = 0;
+    const res = reviews
+      .filter((review) => review.productId === product.id)
+      .map((review) => (total += Number(ratingData.indexOf(ratingData[review.rating]))));
+    return Number((total / res.length).toFixed());
+  };
+
+  return <ReviewContext.Provider value={{ reviews, addReview, calcRating }}>{children}</ReviewContext.Provider>;
+}
+
+export const useReviews = () => useContext(ReviewContext);
+
+export default ReviewProvider;
