0f4d297078b54ae4268430d10b025b983c39b809
osci-the-orca <88oscste@skola.boras.se>
2022-09-14 13:09:21 +0200

    mapping the size[] for selectOptions and handleaddTocart

--- src/components/Drawers/QuickViewDrawer.tsx
+++ src/components/Drawers/QuickViewDrawer.tsx
@@ -1,21 +1,18 @@
 import * as Icon from "@mui/icons-material";
 import { Box, Button, Container, FormControl, IconButton, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
-import styled from "styled-components";
 import { Autoplay, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useCurrency } from "../../contexts/CurrencyContext";
-import { useFavorites } from "../../contexts/FavoriteContext";
 import Product from "../../models/Product";
 
 interface Props {
   product: Product;
   toggleDrawer: React.Dispatch<React.SetStateAction<boolean>>;
   handleAdd: () => void;
-  size: string;
   handleChange: (e: SelectChangeEvent) => void;
+  sizeState: string;
 }
 
-const QuickViewDrawer = ({ product, toggleDrawer, handleAdd, size, handleChange }: Props) => {
+const QuickViewDrawer = ({ product, toggleDrawer, handleAdd, handleChange, sizeState }: Props) => {
   return (
     <Container maxWidth="lg" fixed sx={{ height: "150px", display: "flex", position: "relative", alignItems: "center" }}>
       <IconButton sx={{ position: "absolute", top: 0, right: "1rem" }} onClick={() => toggleDrawer((prev) => !prev)}>
@@ -52,20 +49,28 @@ const QuickViewDrawer = ({ product, toggleDrawer, handleAdd, size, handleChange 
       <Box sx={{ display: "flex", flexDirection: "column", flex: 1, margin: "0 3rem 0 1rem", gap: ".5rem", padding: ".5rem" }}>
         <FormControl size="small">
           <InputLabel>Size</InputLabel>
-          <Select id="demo-simple-select" value={size} label="Size" onChange={handleChange}>
-            <MenuItem value={1}>XS</MenuItem>
-            <MenuItem value={2}>S</MenuItem>
-            <MenuItem value={3}>M</MenuItem>
-            <MenuItem value={4}>L</MenuItem>
-            <MenuItem value={5}>XL</MenuItem>
+          <Select id="demo-simple-select" value={sizeState} label="Size" onChange={handleChange}>
+            {product.size.map((size, index) => (
+              <MenuItem key={index} value={size}>
+                {size}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
-        {size === "" ? (
+        {sizeState === "" ? (
           <Button variant="contained" color="primary" disabled size="small">
             <Typography variant="body2">Pick a size</Typography>
           </Button>
         ) : (
-          <Button variant="contained" color="primary" onClick={() => handleAdd()} size="small">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              handleAdd();
+              toggleDrawer((prev) => !prev);
+            }}
+            size="small"
+          >
             <Typography variant="body2">Add to cart</Typography>
           </Button>
         )}
