6370b045d1ddb10c65d1e80de0b4421d4aac83cd
osci-the-orca <88oscste@skola.boras.se>
2022-09-15 00:49:27 +0200

    added state for opening deleteDialog in admintableMoBILE

--- src/components/Admin/AdminTableMobile.tsx
+++ src/components/Admin/AdminTableMobile.tsx
@@ -14,8 +14,10 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import { useState } from "react";
 import { useProducts } from "../../contexts/ProductContext";
 import Product from "../../models/Product";
+import DeleteDialog from "./DeleteDialog";
 
 interface Props {
   handleEdit: (product: Product) => void;
@@ -28,8 +30,11 @@ interface RowProps {
   handleDelete: (id: string) => void;
 }
 
+// BREAK OUT TO ITS OWN COMPONENT; ADMINTABLE MOBILE ROW
 function Row({ product, handleEdit, handleDelete }: RowProps) {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
 
   return (
     <React.Fragment>
@@ -78,10 +83,11 @@ function Row({ product, handleEdit, handleDelete }: RowProps) {
               <IconButton onClick={() => handleEdit(product)}>
                 <Edit />
               </IconButton>
-              <IconButton onClick={() => handleDelete(product.id)}>
+              <IconButton onClick={() => setDialogOpen(true)}>
                 <DeleteIcon />
               </IconButton>
             </Box>
+            <DeleteDialog isOpen={dialogOpen} handleDelete={() => handleDelete(product.id)} setIsOpen={setDialogOpen} />
           </Collapse>
         </TableCell>
       </TableRow>
