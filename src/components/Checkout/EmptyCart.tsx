4b37a9b31f57a25d5ead16fb0548bdfe3c5be7fb
4l3x <alexander.auermaa@gmail.com>
2022-09-15 00:14:39 +0200

    empty carts has some spice

--- src/components/Checkout/EmptyCart.tsx
+++ src/components/Checkout/EmptyCart.tsx
@@ -1,9 +1,16 @@
+import { Box, Container } from "@mui/material";
 import MainContent from "../MainContent";
+import disappointed from "../Review/img/disappointed.webp";
 
 const EmptyCart = () => {
   return (
     <MainContent>
-    <div>Cart is empty.</div>
+      <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <Box>Cart is empty.</Box>
+        <Box>
+          <img draggable="false" width={32} src={disappointed} />
+        </Box>
+      </Box>
     </MainContent>
     // TODO: Add more content, suggestive things the user can buy blabla
   );
