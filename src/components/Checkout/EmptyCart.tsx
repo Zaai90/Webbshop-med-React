5701381f02f901d8d078c7aaa71e0b5d1aa9aac7
4l3x <alexander.auermaa@gmail.com>
2022-09-15 00:08:02 +0200

    empty cart fix

--- src/components/Checkout/EmptyCart.tsx
+++ src/components/Checkout/EmptyCart.tsx
@@ -1,6 +1,10 @@
+import MainContent from "../MainContent";
+
 const EmptyCart = () => {
   return (
+    <MainContent>
     <div>Cart is empty.</div>
+    </MainContent>
     // TODO: Add more content, suggestive things the user can buy blabla
   );
 };
