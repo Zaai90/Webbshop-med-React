6ef45554744c359737edf8aa908d85a4adaff755
Zaai90 <erik.grune@gmail.com>
2022-09-14 13:49:38 +0200

    fix mapper

--- src/components/ProductMapper.tsx
+++ src/components/ProductMapper.tsx
@@ -1,21 +1,42 @@
-import Product from "../models/Product";
+import { Container, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { useProducts } from "../contexts/ProductContext";
 import GridItem from "./GridItem";
 
-interface Props {
-  products: Product[];
-  value: string;
-}
+const StoreGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.5rem;
+  padding: 1rem 0;
 
-const ProductMapper = ({ products, value }: Props) => {
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+const ProductMapper = () => {
+  const { category } = useParams();
+  const { products } = useProducts();
+  let value = category || "all";
+  const categories = products.filter((x, i) => products.findIndex((y) => x.category === y.category) === i).map((x) => x.category.toLowerCase());
+  categories.push("all");
+
+  if (!categories.includes(value)) {
+    value = "all";
+  }
+
   return (
-    <>
-      {value === "All" && products.map((product) => <GridItem key={product.id} product={product} />)}
-      {products
-        .filter((product) => product.category === value)
-        .map((product) => (
-          <GridItem key={product.id} product={product} />
-        ))}
-    </>
+    <Container>
+      <Typography variant="h4" sx={{ borderBottom: "1px solid rgba(0,0,0,0.35)" }}>
+        {value.split("")[0].toUpperCase() + value.slice(1)}
+      </Typography>
+      <StoreGridStyled>
+        {categories.includes(value) && value !== "all"
+          ? products.filter((product) => product.category.toLowerCase() === value).map((product) => <GridItem key={product.id} product={product} />)
+          : products.map((product) => <GridItem key={product.id} product={product} />)}
+      </StoreGridStyled>
+    </Container>
   );
 };
 
