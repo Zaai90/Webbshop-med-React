be049e3ce20fc0240188b73a02b583f29e970eec
Zaai90 <erik.grune@gmail.com>
2022-09-14 13:48:23 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import ProductMapper from "./components/ProductMapper";
 import GlobalStyle from "./globalStyles";
 import Layout from "./Layout";
 import Admin from "./pages/Admin";
@@ -15,11 +16,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="store" element={<Store />} />
+          <Route path="store/" element={<Store />}>
+            <Route path=":category" element={<ProductMapper />} />
+          </Route>
           <Route path="product/:id" element={<ProductPage />} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="admin" element={<Admin />} />
           <Route path="wishlist" element={<Favorites />} />
+          <Route path="*" element={<h1>404</h1>} />
         </Route>
       </Routes>
     </>
