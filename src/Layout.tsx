040dce0fc9ae8012ad6effc6e4652ed62cf13e85
Zaai90 <erik.grune@gmail.com>
2022-09-14 18:00:17 +0200

    add ScrollToTop Component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import AppBar from "./components/AppBar/AppBar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const Layout = () => {
   return (
     <>
       <AppBar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </>
