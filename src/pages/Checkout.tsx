8749d07ef0e4204a2492b7046e98894bb7dbe19c
4l3x <alexander.auermaa@gmail.com>
2022-09-15 00:29:46 +0200

    leave it for now

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,10 @@
-import EmptyCart from "../components/Checkout/EmptyCart";
 import PaymentProcess from "../components/Checkout/PaymentProcess";
 import MainContent from "../components/MainContent";
-import { useCart } from "../contexts/CartContext";
 
 const Checkout = () => {
-  const { cart } = useCart();
   return (
     <MainContent>
-      {cart.length == 0 ? <EmptyCart /> : <PaymentProcess />}
+      <PaymentProcess />
     </MainContent>
   );
 };
