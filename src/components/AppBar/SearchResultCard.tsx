d4a617fc2758e47e7ea8773bf27eece386d7fc87
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-16 13:35:55 +0200

    larger content in card on tablet+

--- src/components/AppBar/SearchResultCard.tsx
+++ src/components/AppBar/SearchResultCard.tsx
@@ -1,10 +1,15 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import styled from "styled-components";
 import { useCurrency } from "../../contexts/CurrencyContext";
 import Product from "../../models/Product";
+import theme from "../../utils/Theme";
 
 const Thumbnail = styled.img`
   height: 50px;
+
+  @media (min-width: ${theme.breakpoints.values.tablet}px) {
+    height: 100px;
+  }
 `;
 
 interface Props {
@@ -13,17 +18,19 @@ interface Props {
 
 const SearchResultCard = ({ product }: Props) => {
   const { convertToCurrencyValue } = useCurrency();
+  const smScreen = useMediaQuery(theme.breakpoints.down("tablet"));
+
   return (
     <Box width={"100%"} display={"flex"} gap={".5rem"} margin={".5rem 0"}>
       <Box display={"flex"} flex={"0 !important"}>
         <Thumbnail src={product.img[0]} alt={product.title} />
       </Box>
-      <Box display={"flex"} flexDirection={"column"} justifyContent={"space-between"}>
-        <Typography variant={"caption"} textTransform={"uppercase"} fontWeight={"700s"}>
+      <Box display={"flex"} flexDirection={"column"} justifyContent={smScreen ? "space-between" : "center"}>
+        <Typography variant={smScreen ? "caption" : "body1"} textTransform={"uppercase"} fontWeight={"700s"}>
           {product.title}
         </Typography>
-        <Typography variant={"caption"}>{product.designer}</Typography>
-        <Typography variant={"caption"}>{convertToCurrencyValue(product.price)}</Typography>
+        <Typography variant={smScreen ? "caption" : "body1"}>{product.designer}</Typography>
+        <Typography variant={smScreen ? "caption" : "body1"}>{convertToCurrencyValue(product.price)}</Typography>
       </Box>
     </Box>
   );
