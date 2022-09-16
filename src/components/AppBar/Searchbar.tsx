306408935a217d49fdf68655141150e5b46c69e8
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-16 13:39:42 +0200

    fix width, box shadow, absolute pos, position, non-focus

--- src/components/AppBar/Searchbar.tsx
+++ src/components/AppBar/Searchbar.tsx
@@ -1,56 +1,90 @@
 import * as Icon from "@mui/icons-material/";
 import { Box, Collapse, Container, Input, useMediaQuery } from "@mui/material";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 import theme from "../../utils/Theme";
 import SearchResult from "./SearchResult";
 
+const SearchContainer = styled(Box)`
+  position: absolute;
+  bottom: -2.5rem;
+  align-self: center;
+`;
+
+const StyledInput = styled(Input)`
+  border-radius: 5px;
+  padding: 0 1rem;
+  gap: 0.5rem;
+`;
+
 interface Props {
   toggleSearch: React.Dispatch<React.SetStateAction<boolean>>;
-  position?: "miniAppBar" | "AppBar";
+  containerRef: React.RefObject<HTMLDivElement>;
+  onClickOutside: () => void;
+  searchIsActive: boolean;
 }
 
-const Searchbar = React.forwardRef(({ toggleSearch, position }: Props, ref) => {
+const Searchbar = React.forwardRef(({ toggleSearch, containerRef, onClickOutside, searchIsActive }: Props, ref) => {
   const [searchString, setSearchString] = useState("");
-  const smScreen = useMediaQuery(theme.breakpoints.down("tablet"));
 
-  const searchRef = useRef();
+  const smScreen = useMediaQuery(theme.breakpoints.down("tablet"));
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (searchIsActive) {
+      const handleClickOutisde = (e: any) => {
+        if (searchRef.current && !searchRef.current.contains(e.target)) {
+          onClickOutside && onClickOutside();
+        }
+      };
+      document.addEventListener("click", handleClickOutisde, true);
+
+      return () => {
+        document.removeEventListener("click", handleClickOutisde, true);
+      };
+    }
+  }, [onClickOutside]);
+
+  function getHeaderWidth() {
+    return containerRef.current?.clientWidth as number;
+  }
 
   const handleChange = (value: string) => {
     setSearchString(value);
   };
 
-  function clearSearch() {
-    setSearchString("");
-  }
-
   return (
-    <Box ref={ref}>
+    <SearchContainer ref={ref} sx={{ width: getHeaderWidth }}>
       <Container maxWidth="lg" fixed>
-        <Box sx={{ position: "relative" }} paddingLeft={position === "miniAppBar" ? 0 : smScreen ? "100px" : "130px"} alignSelf={["flex-end"]}>
-          <Input
+        <Box sx={{ position: "relative" }} paddingLeft={smScreen ? "80px" : "110px"} alignSelf={["flex-end"]}>
+          <StyledInput
+            // autoFocus
             type="text"
             fullWidth
+            autoFocus
+            disableUnderline
             placeholder="Search..."
             startAdornment={<Icon.Search />}
             onChange={(e) => handleChange(e.target.value)}
             ref={searchRef}
-          ></Input>
+            sx={{ background: theme.palette.common.white, boxShadow: "2px 6px 8px rgba(0, 0, 0, 0.15), -2px 6px 8px rgba(0, 0, 0, 0.15);" }}
+          ></StyledInput>
           <Box
             sx={{
               position: "absolute",
-              width: `${position === "miniAppBar" ? "100%" : `calc(100% - ${smScreen ? "100px" : "130px"})`}`,
+              width: `calc(${getHeaderWidth} - ${smScreen ? "100px" : "130px"})`,
               background: "#f7f7f7",
               zIndex: 1200,
               boxShadow: "2px 6px 8px rgba(0,0,0,.15),-2px 6px 8px rgba(0,0,0,.15)",
             }}
           >
             <Collapse easing={{ enter: "theme.transitions.easing.easeInOut", exit: "theme.transitions.easing.easeInOut" }} in={searchString !== ""}>
-              <SearchResult toggleSearch={toggleSearch} searchString={searchString} />
+              <SearchResult toggleSearch={toggleSearch} searchString={searchString} headerWidth={getHeaderWidth() - (smScreen ? 130 : 160)} />
             </Collapse>
           </Box>
         </Box>
       </Container>
-    </Box>
+    </SearchContainer>
   );
 });
 
